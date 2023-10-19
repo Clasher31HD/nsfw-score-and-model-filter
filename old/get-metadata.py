38b011ff84e38f8309011ffca2021b8e64837736
Clasher31HD <clasher31hd@gmail.com>
2023-10-19 17:01:28 +0200

    update get metadata

--- old/get-metadata.py
+++ old/get-metadata.py
@@ -1,27 +1,17 @@
 from PIL import Image
-from PIL.ExifTags import TAGS, GPSTAGS
 
 
-def get_image_metadata(image_path):
+def get_png_metadata(image_path):
     try:
         with Image.open(image_path) as img:
-            metadata = img._getexif()
-            if metadata:
-                print("Image Metadata:")
-                for tag, value in metadata.items():
-                    tag_name = TAGS.get(tag, tag)
-                    if isinstance(value, bytes):
-                        try:
-                            value = value.decode('utf-8')
-                        except UnicodeDecodeError:
-                            pass
-                    print(f"{tag_name}: {value}")
-            else:
-                print("No metadata found in the image.")
+            info = img.info
+            print("PNG Metadata:")
+            for key, value in info.items():
+                print(f"{key}: {value}")
     except Exception as e:
         print(f"An error occurred: {str(e)}")
 
 
 if __name__ == "__main__":
     image_path = "path_to_your_image.jpg"  # Replace with the path to your image
-    get_image_metadata(image_path)
+    get_png_metadata(image_path)
