ed4fa2b306063049557d2583e93178b304cb6ec2
Clasher31HD <clasher31hd@gmail.com>
2023-10-20 08:31:24 +0200

    update getmetadata

--- get-metadata.py
+++ get-metadata.py
@@ -1,4 +1,5 @@
 from PIL import Image
+import os
 
 
 def get_png_metadata(image_path):
@@ -8,6 +9,15 @@ def get_png_metadata(image_path):
             print("PNG Metadata:")
             for key, value in info.items():
                 print(f"{key}: {value}")
+
+        file_name = os.path.basename(image_path)
+        directory = os.path.dirname(image_path)
+        file_size = os.path.getsize(image_path)
+
+        print("\nAdditional File Information:")
+        print(f"File Name: {file_name}")
+        print(f"Directory: {directory}")
+        print(f"File Size: {file_size} bytes")
     except Exception as e:
         print(f"An error occurred: {str(e)}")
 
