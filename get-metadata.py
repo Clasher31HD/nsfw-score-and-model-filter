68b2242cbb0aa49ed5422f4c09c0ee78815dba35
Clasher31HD <clasher31hd@gmail.com>
2023-10-20 08:40:35 +0200

    improved getmetadata

--- get-metadata.py
+++ get-metadata.py
@@ -4,29 +4,34 @@ import os
 
 def get_image_metadata(image_path):
     try:
-        metadata_list = []
+        metadata_dict = {
+            "parameter": {},
+            "File Name": "",
+            "Directory": "",
+            "File Size": 0
+        }
 
         with Image.open(image_path) as img:
             info = img.info
             for key, value in info.items():
-                metadata_list.append(f"{key}: {value}")
+                if key in ["Negative prompt", "Steps", "Sampler", "CFG Scale", "Seed", "Model hash", "Model",
+                           "Seed resize from", "Denoising strength", "Version"]:
+                    metadata_dict["Positive prompt"][key] = value
+                else:
+                    metadata_dict[key] = value
 
         file_name = os.path.basename(image_path)
         directory = os.path.dirname(image_path)
         file_size = os.path.getsize(image_path)
 
-        additional_info = [
-            f"File Name: {file_name}",
-            f"Directory: {directory}",
-            f"File Size: {file_size} bytes"
-        ]
+        metadata_dict["File Name"] = file_name
+        metadata_dict["Directory"] = directory
+        metadata_dict["File Size"] = file_size
 
-        metadata_list.extend(additional_info)
-
-        return metadata_list
+        return metadata_dict
     except Exception as e:
         print(f"An error occurred: {str(e)}")
-        return []
+        return {}
 
 
 if __name__ == "__main__":
@@ -35,5 +40,10 @@ if __name__ == "__main__":
 
     if all_metadata:
         print("All Metadata Values:")
-        for item in all_metadata:
-            print(item)
+        for key, value in all_metadata.items():
+            if isinstance(value, dict):
+                print(f"{key}:")
+                for subkey, subvalue in value.items():
+                    print(f"  {subkey}: {subvalue}")
+            else:
+                print(f"{key}: {value}")
