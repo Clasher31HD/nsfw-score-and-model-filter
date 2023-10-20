828ff30fd4dbc1cb71a607fd6d9a6590d5512599
Clasher31HD <clasher31hd@gmail.com>
2023-10-20 08:32:29 +0200

    improved getmetadata

--- get-metadata.py
+++ get-metadata.py
@@ -2,26 +2,38 @@ from PIL import Image
 import os
 
 
-def get_png_metadata(image_path):
+def get_image_metadata(image_path):
     try:
+        metadata_list = []
+
         with Image.open(image_path) as img:
             info = img.info
-            print("PNG Metadata:")
             for key, value in info.items():
-                print(f"{key}: {value}")
+                metadata_list.append(f"{key}: {value}")
 
         file_name = os.path.basename(image_path)
         directory = os.path.dirname(image_path)
         file_size = os.path.getsize(image_path)
 
-        print("\nAdditional File Information:")
-        print(f"File Name: {file_name}")
-        print(f"Directory: {directory}")
-        print(f"File Size: {file_size} bytes")
+        additional_info = [
+            f"File Name: {file_name}",
+            f"Directory: {directory}",
+            f"File Size: {file_size} bytes"
+        ]
+
+        metadata_list.extend(additional_info)
+
+        return metadata_list
     except Exception as e:
         print(f"An error occurred: {str(e)}")
+        return []
 
 
 if __name__ == "__main__":
-    image_path = "path_to_your_image.jpg"  # Replace with the path to your image
-    get_png_metadata(image_path)
+    image_path = "path_to_your_image.png"  # Replace with the path to your PNG image
+    all_metadata = get_image_metadata(image_path)
+
+    if all_metadata:
+        print("All Metadata Values:")
+        for item in all_metadata:
+            print(item)
