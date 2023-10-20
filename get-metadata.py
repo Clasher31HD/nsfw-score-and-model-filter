d34f06451d2e20144729148f02d6b662c21750ac
Clasher31HD <clasher31hd@gmail.com>
2023-10-20 08:47:21 +0200

    improved getmetadata

--- get-metadata.py
+++ get-metadata.py
@@ -1,49 +1,26 @@
 from PIL import Image
-import os
 
 
-def get_image_metadata(image_path):
-    try:
-        metadata_dict = {
-            "parameter": {},
-            "File Name": "",
-            "Directory": "",
-            "File Size": 0
-        }
+def extract_metadata_from_parameter(metadata_str):
+    metadata_dict = {"Positive prompt": ""}
+    metadata_str = metadata_str.split('.')
 
-        with Image.open(image_path) as img:
-            info = img.info
-            for key, value in info.items():
-                if key in ["Negative prompt", "Steps", "Sampler", "CFG Scale", "Seed", "Model hash", "Model",
-                           "Seed resize from", "Denoising strength", "Version"]:
-                    metadata_dict["Positive prompt"][key] = value
-                else:
-                    metadata_dict[key] = value
+    for segment in metadata_str:
+        key_value = segment.split(': ')
+        if len(key_value) == 2:
+            key, value = key_value[0], key_value[1]
+            if key == "Positive prompt":
+                metadata_dict["Positive prompt"] = value
+            else:
+                metadata_dict[key] = value
 
-        file_name = os.path.basename(image_path)
-        directory = os.path.dirname(image_path)
-        file_size = os.path.getsize(image_path)
-
-        metadata_dict["File Name"] = file_name
-        metadata_dict["Directory"] = directory
-        metadata_dict["File Size"] = file_size
-
-        return metadata_dict
-    except Exception as e:
-        print(f"An error occurred: {str(e)}")
-        return {}
+    return metadata_dict
 
 
 if __name__ == "__main__":
-    image_path = "path_to_your_image.png"  # Replace with the path to your PNG image
-    all_metadata = get_image_metadata(image_path)
+    metadata_str = "Hallo, ich, bin cool.Negative prompt: Ugly.Steps: 50.Sampler: Euler"
+    metadata = extract_metadata_from_parameter(metadata_str)
 
-    if all_metadata:
-        print("All Metadata Values:")
-        for key, value in all_metadata.items():
-            if isinstance(value, dict):
-                print(f"{key}:")
-                for subkey, subvalue in value.items():
-                    print(f"  {subkey}: {subvalue}")
-            else:
-                print(f"{key}: {value}")
+    print("Metadata:")
+    for key, value in metadata.items():
+        print(f"{key}: {value}")
