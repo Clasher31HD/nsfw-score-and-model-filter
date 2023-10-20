626d900d6e90d1e0d638ee0a83e2a5f7523eb2da
Clasher31HD <clasher31hd@gmail.com>
2023-10-20 10:35:08 +0200

    fix

--- extract-metadata-and-insert-into-database-mysql.py
+++ extract-metadata-and-insert-into-database-mysql.py
@@ -22,7 +22,7 @@ def extract_metadata_from_parameter(metadata_str, image_path):
     directory = os.path.dirname(image_path)
     file_size = os.path.getsize(image_path)
     metadata_dict["File Name"] = file_name.strip(".png")
-    metadata_dict["Directory"] = directory
+    metadata_dict["Directory"] = directory.rsplit('/', 2)[-2]
     metadata_dict["File Size"] = file_size
 
     # Split by the first occurrence of "Negative prompt"
